964d733051194d2876f33f243a62d1c8385e1f02
Marcus <marcus.faulstich@tu-ilmenau.de>
2025-01-14 17:34:00 +0100

    license checker is now able to purge license text from files

--- main.go
+++ main.go
@@ -13,29 +13,39 @@ import (
 //go:embed license_text
 var licenseText string
 
+var pathFlag string
+var recursiveFlag bool
+var fileEndingsFlag string
+var scrapeLicenseFlag string
+var dryRunFlag bool
+var textFileFlag string
+var updateFlag bool
+var purgeFlag bool
+
 func main() {
-	pathFlag := flag.String("d", ".", "Path to the directory to check")
-	recursiveFlag := flag.Bool("r", false, "Check the directory recursively")
-	fileEndingsFlag := flag.String("e", "", "Comma separated list of file endings to check")
-	scrapeLicenseFlag := flag.String("s", "", "URL to scrape the license from")
-	dryRunFlag := flag.Bool("v", false, "Perform a dry run to check which files would be checked")
-	textFileFlag := flag.String("t", "", "Path to a text file containing the license text")
-	updateFlag := flag.Bool("u", false, "Update the license text")
+	flag.StringVar(&pathFlag, "d", ".", "Path to the directory to check")
+	flag.BoolVar(&recursiveFlag, "r", false, "Check the directory recursively")
+	flag.StringVar(&fileEndingsFlag, "e", "", "Comma separated list of file endings to check")
+	flag.StringVar(&scrapeLicenseFlag, "s", "", "URL to scrape the license from")
+	flag.BoolVar(&dryRunFlag, "v", false, "Perform a dry run to check which files would be checked")
+	flag.StringVar(&textFileFlag, "t", "", "Path to a text file containing the license text")
+	flag.BoolVar(&updateFlag, "u", false, "Update the license text")
+	flag.BoolVar(&purgeFlag, "p", false, "Purge the license text")
 	flag.Parse()
 
-	fileTypes := strings.Split(*fileEndingsFlag, ",")
-	path, ok := processPath(*pathFlag)
+	fileTypes := strings.Split(fileEndingsFlag, ",")
+	path, ok := processPath(pathFlag)
 	if !ok {
 		println("Path not ok. Please enter a valid path.")
 		return
 	}
-	err := setLicenseText(*scrapeLicenseFlag, *textFileFlag)
+	err := setLicenseText()
 	if err != nil {
 		println("Something went wrong setting the license text.")
 		panic(err)
 	}
 
-	changedFiles, failedFiles, err := checkDirectory(path, fileTypes, *recursiveFlag, *dryRunFlag, *updateFlag)
+	changedFiles, failedFiles, err := checkDirectory(path, fileTypes)
 	if err != nil {
 		println("Something bad happened.")
 		println(err)
@@ -47,13 +57,13 @@ func main() {
 	println()
 	println("Failed to Change ", len(failedFiles), " files")
 	println(strings.Join(failedFiles, "\n"))
-	if *dryRunFlag {
+	if dryRunFlag {
 		println()
 		println("This was a dry run. No files were changed.")
 	}
 }
 
-func checkDirectory(path string, fileTypes []string, recursively bool, dryrun bool, update bool) ([]string, []string, error) {
+func checkDirectory(path string, fileTypes []string) ([]string, []string, error) {
 	println("Checking Directory: ", path)
 	entries, err := os.ReadDir(path)
 	if err != nil {
@@ -65,8 +75,8 @@ func checkDirectory(path string, fileTypes []string, recursively bool, dryrun bo
 		filePath := path + file.Name()
 
 		if file.IsDir() {
-			if recursively {
-				changedFiles, failedFiles, err := checkDirectory(filePath, fileTypes, recursively, dryrun, update)
+			if recursiveFlag {
+				changedFiles, failedFiles, err := checkDirectory(filePath, fileTypes)
 				if err == nil {
 					changedFileNames = append(changedFileNames, changedFiles...)
 					failedFileNames = append(failedFileNames, failedFiles...)
@@ -74,7 +84,6 @@ func checkDirectory(path string, fileTypes []string, recursively bool, dryrun bo
 			}
 			continue
 		}
-
 		if len(fileTypes) > 0 {
 			var ok bool = false
 			for _, typ := range fileTypes {
@@ -88,20 +97,26 @@ func checkDirectory(path string, fileTypes []string, recursively bool, dryrun bo
 				continue
 			}
 		}
-
 		content, err := os.ReadFile(filePath)
 		if err != nil {
 			failedFileNames = append(failedFileNames, filePath)
 			continue
 		}
-
-		newContent, exists := addLicense(content, update)
-		if exists {
-			// If the license notifier already exists, don't add it
-			continue
+		var newContent []byte
+		if purgeFlag {
+			var exists bool
+			newContent, exists = purgeLicense(content)
+			if !exists {
+				continue
+			}
+		} else {
+			var exists bool
+			newContent, exists = addLicense(content)
+			if exists {
+				continue
+			}
 		}
-
-		if dryrun {
+		if dryRunFlag {
 			f, err := os.OpenFile(filePath, os.O_RDWR, 0666)
 			f.Close()
 			if err != nil {
@@ -116,23 +131,36 @@ func checkDirectory(path string, fileTypes []string, recursively bool, dryrun bo
 			}
 		}
 		changedFileNames = append(changedFileNames, filePath)
-
 	}
 	return changedFileNames, failedFileNames, nil
 }
 
-func addLicense(content []byte, update bool) ([]byte, bool) {
+func addLicense(content []byte) ([]byte, bool) {
 	exists := strings.HasPrefix(string(content), licenseText)
 	if exists {
 		return content, exists
 	}
-	if !update {
+	if !updateFlag {
 		return append([]byte(licenseText), content...), false
 	}
 
 	license_end_marker := "********************************************************/\n"
-	contentNoLicense := strings.SplitN(string(content), license_end_marker, 2)[1]
+	splitContent := strings.SplitN(string(content), license_end_marker, 2)
+	if len(splitContent) < 2 {
+		return append([]byte(licenseText), content...), false
+	}
+	contentNoLicense := splitContent[1]
 	return append([]byte(licenseText), []byte(contentNoLicense)...), false
+}
+
+func purgeLicense(content []byte) ([]byte, bool) {
+	license_end_marker := "********************************************************/\n"
+	splitContent := strings.SplitN(string(content), license_end_marker, 2)
+	if len(splitContent) < 2 {
+		return content, false
+	}
+	contentNoLicense := splitContent[1]
+	return []byte(contentNoLicense), true
 }
 
 func processPath(rawPath string) (string, bool) {
@@ -148,13 +176,12 @@ func processPath(rawPath string) (string, bool) {
 	return path, true
 }
 
-func setLicenseText(scrapeUrl string, filePath string) error {
-	if filePath != "" && scrapeUrl != "" {
+func setLicenseText() error {
+	if textFileFlag != "" && scrapeLicenseFlag != "" {
 		return errors.New("Please provide either a file path or a scrape url")
 	}
-	if scrapeUrl != "" {
-
-		res, err := http.Get(scrapeUrl)
+	if scrapeLicenseFlag != "" {
+		res, err := http.Get(scrapeLicenseFlag)
 		if err != nil {
 			return err
 		}
@@ -163,8 +190,8 @@ func setLicenseText(scrapeUrl string, filePath string) error {
 			return err
 		}
 		licenseText = string(text)
-	} else if filePath != "" {
-		contents, err := os.ReadFile(filePath)
+	} else if textFileFlag != "" {
+		contents, err := os.ReadFile(textFileFlag)
 		if err != nil {
 			return err
 		}
